c898a5e4895b61bca4c998ab5d18e9864d6cbf41
Francisco Juan <juansoussev@gmail.com>
2025-06-13 17:26:37 -0300

    feat: creating DailyScrum Model and a Sample

---
+++ Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,16 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Francisco Juan on 13/06/25.
+//
+
+import Foundation
+import ThemeKit
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
