a60d2ad797b7ce7508754dca48890ae2e74d1c8a
Francisco Juan <juansoussev@gmail.com>
2025-06-18 13:01:40 -0300

    feat: creating DailyScrumDetail

---
+++ Scrumdinger/Views/DetailView.swift
@@ -0,0 +1,43 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Francisco Juan on 18/06/25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        List {
+            Section(header: Text("Meeting Info")) {
+                Label("Start meeting", systemImage: "timer")
+                    .font(.headline)
+                    .foregroundColor(.accentColor)
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                .accessibilityElement(children: .combine)
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        DetailView(scrum: DailyScrum.sampleData[0])
+    }
+}
